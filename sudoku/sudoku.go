ad9b6129746b887caf0216be3263464e6e6bf029
Draculk <kayranvieira456@hotmail.com>
2021-04-17 15:02:05 -0300

    :sparkles: várias funções.

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
-	"errors"
 )
 
 var (
@@ -120,15 +120,15 @@ func getChecagemElementos() map[string][]string {
 	return peersFinal
 }
 
-// Converte o grid em um dicionário de {quadrado: caractere} com '.' para as casas vazias.
-func valoresGrid(grid string) (map[string]string, error){
+// Converte o grid em um dicionário de {quadrado: caractere} com '.' ou '0' para as casas vazias.
+func valoresGrid(grid string) (map[string]string, error) {
 	caracteres := []string{}
 	tamanhoGrid := len(grid)
 	tamanhoCaracteres := len(caracteres)
 
-	for iterador := 0; iterador < tamanhoGrid; iterador++{
+	for iterador := 0; iterador < tamanhoGrid; iterador++ {
 		verifica := string(grid[iterador : iterador+1])
-		if strings.Contains(digitos, verifica) || verifica == "0" || verifica == "."{
+		if strings.Contains(digitos, verifica) || verifica == "0" || verifica == "." {
 			caracteres = append(caracteres, verifica)
 		}
 	}
@@ -146,4 +146,87 @@ func valoresGrid(grid string) (map[string]string, error){
 	}
 
 	return novoGrid, nil
+}
+
+func assign(valores map[string]string, index string, valor string) error {
+	outrosValores := strings.Replace(valores[index], valor, "", -1)
+	for _, tempValor := range outrosValores {
+		if err := eliminar(valores, index, string(tempValor)); err != nil {
+			return err 
+		}
+	}
+
+	return nil
+
+}
+
+func eliminar(valores map[string]string, index string, valor string) error{
+	//Já eliminado
+	if !strings.Contains(valores[index], valor){
+		return nil
+	}
+	valores[index] = strings.Replace(valores[index], valor, "", -1)
+
+	tamanhoValores := len(valores[index])
+
+	if tamanhoValores == 0 {
+		return errors.New("O último valor foi removido do map de valores")
+	} else if tamanhoValores == 1 {
+		tempValor := valores[index]
+		tudoEliminado := true
+		for _, iterador := range checagemElementos[index]{
+			err := eliminar(valores, iterador, tempValor)
+			if err != nil {
+				tudoEliminado = false
+			}
+		}
+
+		if !tudoEliminado {
+			return errors.New("Nem todos foram eliminados")
+		}
+	}
+	//Quando a unidade for reduzida para apenas um espaço, checa se pode ser colocada lá.
+	for _, unidade := range encontraCelula[index]{
+		espaco := []string{}
+		for _, unidade2 := range unidade {
+			if strings.Contains(valores[unidade2], valor){
+				espaco = append(espaco, unidade2)
+			}
+		}
+
+		if len(espaco) == 0 {
+			return fmt.Errorf("Não há espaço para: %s", valor)
+		}
+
+		if len(espaco) == 1 {
+			if err := assign(valores, espaco[0], valor); err != nil {
+				return errors.New("Esse valor pode ser colocado aqui.")
+			}
+		}
+	}
+	return nil
+}
+
+func analisaGrid(grid string) (map[string]string, error) {
+	valores := make(map[string]string, len(quadrados))
+	for _, valor := range quadrados {
+		valores[valor] = digitos
+	}
+
+	novoGrid, err := valoresGrid(grid)
+	if err != nil {
+		return nil, err
+	}
+
+	for index, valor := range novoGrid{
+
+		if strings.Contains(digitos, valor){
+			err := assign(valores, index, valor)
+			if err != nil{
+				return nil, err
+			}
+		}
+	}
+
+	return valores, nil
 }
