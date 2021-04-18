32e4b87cc9e52f9c004bb08b0fa7c966dd56a036
Draculk <kayranvieira456@hotmail.com>
2021-04-18 03:24:07 -0300

    :sparkles: main e back funcionando.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DraculK/Sudoku
+
+go 1.16
---
+++ main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/DraculK/Sudoku/sudoku"
+)
+
+func main() {
+	puzzle := "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
+
+	inicio := time.Now()
+	fmt.Println("Resolvendo..")
+	resultado, err := sudoku.Resolve(puzzle)
+	if err != nil {
+		fmt.Println(err)
+	}
+	final := time.Since(inicio)
+	fmt.Println(final)
+	fmt.Println(resultado)
+}
+
